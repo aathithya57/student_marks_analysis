606c9c30cb5fbf8634a761315a72358334970380
Aathithya G <aathithyashivam@gmail.com>
2025-12-16 12:16:22 +0530

    Added python code for marks analysis

---
+++ marks_analysis.py
@@ -0,0 +1,9 @@
+marks = [78, 85, 90, 66, 72]
+
+average = sum(marks) / len(marks)
+highest = max(marks)
+lowest = min(marks)
+
+print("Average Marks:", average)
+print("Highest Marks:", highest)
+print("Lowest Marks:", lowest)
